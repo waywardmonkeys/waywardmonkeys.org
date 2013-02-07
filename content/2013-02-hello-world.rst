46549c8e7bda1b455fd6fd85464aa8876b1191c4
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-02-07 17:14:22 +0700

    Initial commit.

---
+++ content/2013-02-hello-world.rst
@@ -0,0 +1,8 @@
+Hello, World!
+#############
+
+:author: Bruce Mitchener, Jr.
+:date: 2013-02-07
+
+We're just getting started here and we'll have some more content
+coming on to the site in the coming days.
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Bruce Mitchener, Jr."
+SITENAME = u"Notes from a Wayward Monkey"
+SITEURL = 'http://waywardmonkeys.org'
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = 'en'
+
+DEFAULT_CATEGORY = 'General'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+# Pages
+PAGES = ()
+
+# Blogroll
+LINKS =  (('Dylan Foundry', 'http://dylanfoundry.org/'),)
+
+TWITTER_USERNAME = 'ArmyOfBruce'
+
+DEFAULT_PAGINATION = 10
+
+SUMMARY_MAX_LENGTH = None
+
+THEME = 'theme'
